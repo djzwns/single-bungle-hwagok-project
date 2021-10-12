c8fbec9d1c14d5d239d58064b576144c7fdabad9
hjjshg <hjjshg@game.hoseo.edu>
2021-10-12 19:22:59 +0900

    Add: haarcascade dataset
      * dataset & code added.

--- raspberry-image-processing/secure_cam.py
+++ raspberry-image-processing/secure_cam.py
@@ -1,35 +1,49 @@
+import sys
 import cv2
+import numpy as np
 from client import *
 
-STREAM_URL = "http://192.168.10.93:8090/?action=stream"
-HOST = "192.168.10.88"
-PORT = 5000
-client = Client(HOST, PORT)
-client.init()
-#client.sendMsg("[admin]test")
-client.run()
-
+STREAM_URL = "http://127.0.0.1:8090/?action=stream"
+# HOST = "192.168.10.88"
+# PORT = 5000
 
 cap = cv2.VideoCapture(STREAM_URL)
+if not cap.isOpened():
+	print("video open failed")
+	sys.exit()
 
-frameRate = 33
+status, frame = cap.read()
+if not status:
+	print("video capture failed")
+	sys.exit()
+
+# client = Client(HOST, PORT)
+# client.init()
+# client.run()
+
+SCALE_FACTOR = 1.2
+FRAME_RATE = 33
 frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frameSize = (frameWidth, frameHeight)
 print ('frameSize={}'.format(frameSize))
 
+cascade = cv2.CascadeClassifier("./haarcascades/haarcascade_upperbody.xml")
+
 while True:
-	retval, frame = cap.read()
-	if not(retval):
+	status, frame = cap.read()
+	if not status:
 		break
 
-	cv2.imshow('frame', frame)
-	key = cv2.waitKey(frameRate)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	body = cascade.detectMultiScale(gray, SCALE_FACTOR, 2)
+	for (x, y, w, h) in body:
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
 
-	if key == 27:
+	cv2.imshow('cctv', frame)
+
+	if cv2.waitKey(FRAME_RATE) == 27:
 		break
 
-if cap.isOpened():
-	cap.release()
-
+cap.release()
 cv2.destroyAllWindows()
