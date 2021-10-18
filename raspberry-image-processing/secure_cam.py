997b1bd6e5521d576cf635bb6583b0e30d02757b
hjjshg <hjjshg@game.hoseo.edu>
2021-10-18 19:18:27 +0900

    STOP: not work
      * mobilenet 추가중.. 아직 미완

--- raspberry-image-processing/secure_cam.py
+++ raspberry-image-processing/secure_cam.py
@@ -1,6 +1,8 @@
 import sys
+from threading import currentThread
 import cv2
 import numpy as np
+import time
 from client import *
 
 STREAM_URL = "http://127.0.0.1:8090/?action=stream"
@@ -30,12 +32,25 @@ print ('frameSize={}'.format(frameSize))
 
 cascade = cv2.CascadeClassifier("./haarcascades/haarcascade_upperbody.xml")
 
+fps = 1.0 / 33.0 # 초당 33 프레임
+currentTime = 0
+prevTime = 0
+
 while True:
 	status, frame = cap.read()
 	if not status:
 		break
 
+	currentTime = time.time() - prevTime
+	if currentTime < fps:
+		continue
+
+	prevTime = currentTime
+	currentTime -= fps	# 초과된 시간 유지
+
+	# 영상 처리 시작
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#gray = cv2.resize(gray, None, fx=)
 	body = cascade.detectMultiScale(gray, SCALE_FACTOR, 2)
 	for (x, y, w, h) in body:
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
